d6894dafb0106cc916d3d441f40d63b0b71ac921
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-12 15:11:47 -0500

    Initialize test food_truck

---
+++ test/food_truck_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+# require './lib/item'
+require './lib/food_truck'
+
+class FoodTruckTest < Minitest::Test
+  def test_it_exists_and_has_readable_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    assert_instance_of FoodTruck, food_truck
+    assert_equal "Rocky Mountain Pies", food_truck.name
+  end
+end
+
+
+# item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+# item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+# item2.name #=> "Apple Pie (Slice)"
+# item2.price #=> 2.50
