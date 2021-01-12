b5aa0a8af07dc89f5021b6c83ff51c98839884eb
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-12 17:02:30 -0500

    Add final event feature

--- lib/event.rb
+++ lib/event.rb
@@ -1,7 +1,26 @@
 class Event
-  attr_reader :name
+  attr_reader :name,
+              :food_trucks
 
   def initialize(name)
-    @name = name 
+    @name        = name
+    @food_trucks = []
+  end
+
+  def add_food_truck(food_truck)
+    food_trucks << food_truck
+  end
+
+  def food_truck_names
+    food_trucks.collect do |food_truck|
+      food_truck.name
+    end
+  end
+
+  def food_trucks_that_sell(item)
+    # "...takes an argument of an item represented as a String"
+    food_trucks.find_all do |food_truck|
+      food_truck.inventory.include?(item)
+    end
   end
 end
