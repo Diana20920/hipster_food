ab1c8e8c99f8503b6864df469ef8f59880e696b4
Diana Buffone <diana.pamelaet@gmail.com>
2021-01-12 16:28:07 -0500

    Update food_truck and item features iteration 1

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,7 +1,21 @@
 class FoodTruck
-  attr_reader :name
+  attr_reader :name,
+              :inventory
 
   def initialize(name)
-    @name = name 
+    @name      = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    return 0 if inventory.empty?
+      inventory.sum do |name, quantity|
+        quantity
+      end
+
+  end
+
+  def stock(item, quantity)
+    inventory[item] += quantity
   end
 end
